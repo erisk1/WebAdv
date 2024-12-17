55606fe8cac09435305f3d69c5a0419894abd68b
erisk1 <eriskarabeg@gmail.com>
2024-12-17 07:12:43 -0800

    changes

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+console.log('hello world')
+
+
+var b = 28;
+var c = 30;
+var D = b * c;
+var d = null;
+
+var n = 99;
+n++;
+console.log(n);
+
+
+console.log(n);
+
+
+
+var a = 50 +100;
+console.log(a);
