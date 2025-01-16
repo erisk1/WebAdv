697f4c806a0eb32fac52eb0d78f2d7aba705f16b
erisk1 <eriskarabeg@gmail.com>
2025-01-16 16:15:09 +0100

    lesson29

---
+++ javascript/lesson28.js
@@ -0,0 +1,13 @@
+var button3 = document.getElementById('brn2')
+var button4 = document.getElementById('btn3')
+var v_button = document.getElementById('button1')
+var v_text = document.getElementById('txt1')
+
+// v_button.onclick = function(){
+//     v_text.style.color="red";
+//     v_text.style.textAlign.backgroundcolor = "lightgrey";
+//     v_text.style.textAlign = "center";
+//     v_text.style.fontSize= "100px";
+//     v_text.style.
+// }
+ 
---
+++ javascript/lesson29.js
@@ -0,0 +1,10 @@
+var programminlanguages = ['PRISHTINA' , 'vllaznimi' , 'anti shumar']
+
+console.log(programminlanguages)
+programminlanguages.unshift('Ruby on ralls')
+programminlanguages.splice('Ruby')
+
+console.log(math.random*[5])
+
+var numbers = [1,2,3,4,5,6,7,8]
+console.log(first, second, ...othernumber)= numbers
