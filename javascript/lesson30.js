75c1be49fcdbbae1c4d9043a2e038fb9e80b1ae5
erisk1 <eriskarabeg@gmail.com>
2025-01-21 16:15:53 +0100

    lesson30

---
+++ javascript/lesson30.js
@@ -0,0 +1,10 @@
+var input = document.getElementById("input");
+var button = document.getElementById("btn");
+var text = document.getElementById("h1");
+
+button.onclick = function(){
+    text.innerHTML = (input.value >10)
+        h1.innerHTML= "input value is greater than 10";
+
+        
+}
