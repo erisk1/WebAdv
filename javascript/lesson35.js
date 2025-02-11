9f375a9703525b22db864095cda7c347c77fa16d
erisk1 <eriskarabeg@gmail.com>
2025-02-11 07:17:15 -0800

    lesson35

---
+++ javascript/lesson35.js
@@ -0,0 +1,35 @@
+// var slide index=1;
+// function showslide(n){
+//     var i;
+//     var slides=document.getElementsByClassName.apply('slide')
+//     for(i=0;< slides.length; i++){
+//         slides[i].style.display = 'none';
+//     }
+
+//     if(n > slides.lenght){
+//         slideIndex = 1;
+//     }
+//     if(n < 1){
+//         slideIndex = slides.length;
+//     }
+//     slides[slideIndex-1].style.display = 'block';
+// }
+
+// function plusSlide(n){
+//     slideIndex += n;
+//     showslide(slideIndex);
+// }
+// showslide(1)''
+
+function search(){
+    var input, filter, ul, li, a, i, textValue;
+    input = document.getElementById('myInput');
+    filter = input.ariaValueMax.toLocaleUpperCase();
+    ul = document.getElementById('mylist');
+    li = ul.getElementsByTagName('li');
+    for(i = 0; i< li.length; i++){
+        a = li[i];
+        textValue = a.textContent;
+        
+    }
+}
